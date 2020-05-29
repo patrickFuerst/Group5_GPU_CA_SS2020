bf163889688a7129892313861ecce6a09f7abd40
Patrick Fürst <pat.fuerst@gmail.com>
2020-05-29 17:20:53 +0200

    make thrust with cpp working

---
+++ apps/shortestPathFinderThrust/floydWarshallThrust.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <thrust/host_vector.h>
+
+
+void sort_on_device(thrust::host_vector<int>& h_vec);
---
+++ apps/shortestPathFinderThrust/main.cpp
@@ -0,0 +1,56 @@
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+
+#include <thrust/copy.h>
+#include <thrust/fill.h>
+#include <thrust/sequence.h>
+
+#include <iostream>
+#include <fstream>
+#include <limits>
+
+#include "floydWarshallThrust.h"
+#include "OurHelper.h"
+
+int main(int argc, char **argv)
+{
+    std::cout << "Thrust v " << THRUST_MAJOR_VERSION << "." << THRUST_MINOR_VERSION << std::endl;
+
+
+    auto path = evaluateArgs(argc, argv);
+    auto graphFiles = getGraphFiles(path);
+
+
+	for (auto filePath : graphFiles) {
+
+		OurGraph graph = OurGraph::loadGraph(filePath);
+		auto m = graph.getAdjacencyMatrixHostVector();
+
+		//for (int i = 0; i < graph.mNumVertices; i++) {
+		//	for (int j = 0; j < graph.mNumVertices; j++) {
+		//		std::cout <<  m[i * graph.mNumVertices + j] << " ";
+		//	}
+		//	std::cout << std::endl;
+
+		//}
+
+		// Record start time
+		// We actually just track the alorithm implementation 
+		// not data loading 
+		auto start = std::chrono::high_resolution_clock::now();
+
+
+		sort_on_device(m);
+		// print sorted array
+		thrust::copy(m.begin(), m.end(), std::ostream_iterator<int>(std::cout, "\n"));
+
+		// Record end time
+		auto finish = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double, std::milli> elapsed = finish - start;
+		std::cout << "Took " << elapsed.count() << " ms." << std::endl;
+
+
+	}
+
+	
+}
