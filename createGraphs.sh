1e808bef425cc935c67d34736c7f3909fa8c5e0d
Patrick Fürst <pat.fuerst@gmail.com>
2020-06-11 16:52:40 +0200

    add script to create all graphs

---
+++ createGraphs.sh
@@ -0,0 +1,22 @@
+
+
+num_nodes=(10 1000 )
+graph_densities=(0.1 0.3 0.5 0.7 0.9 1.0)
+
+low_weight=1
+high_weight=100
+
+echo "Creating graphs!"
+
+cd data/graphs
+
+for nodes in "${num_nodes[@]}"; do
+	for density in "${graph_densities[@]}"; do
+
+		echo  "Create graph with $nodes nodes and $density density"
+		../../release/graphGenerator -p $nodes $density $low_weight $high_weight
+		echo "----------------------"
+	done 
+done
+
+echo "FINISHED creating graphs!"
