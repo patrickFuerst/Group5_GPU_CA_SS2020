8a5a50ff17ee530ee914eba1b1391d35527456d2
patrickFuerst <pat.fuerst@gmail.com>
2020-06-12 13:00:26 +0200

    Fix typos

--- build.sh
+++ build.sh
@@ -5,14 +5,9 @@ rm -rf ./build
 
 echo "Build all applications!"
 
-
 mkdir build 
 cmake -B "./build" 
-
-D
-D
 cmake --build  build  --config Release
-
 
 echo "Install all applications" 
 cmake --install build --config Release 
