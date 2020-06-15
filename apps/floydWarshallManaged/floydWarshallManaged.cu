e399fa668b698f488db440cff530236e571e7315
panoslantavos <plantavos@csd.auth.gr>
2020-06-15 03:39:20 +0300

    Implement version using unified memory interface

---
+++ apps/floydWarshallManaged/floydWarshallManaged.cu
@@ -0,0 +1,58 @@
+#include "floydWarshallManaged.h"
+#include <thrust/device_vector.h>
+#include <chrono>
+
+#include <cuda.h>
+
+#define BLOCKSIZE 256
+
+// Doesn't do negative edges!!
+__global__
+void iterKernel(int k, int *distances, int N) {
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y;
+
+    // If we're over the edge of the matrix return
+    if ((col >= N) || (distances[N * row + k] == INT_MAX / 2) || (distances[k * N + col] == INT_MAX / 2)) {
+        return;
+    }
+
+    // Otherwise, calculate the distance
+    int candidateBetterDistance = distances[N * row + k] + distances[k * N + col];
+    if (candidateBetterDistance < distances[N * row + col])
+        distances[N * row + col] = candidateBetterDistance;
+}
+
+int iDivUp(int a, int b)
+{
+    return (a % b != 0) ? (a / b + 1) : (a / b);
+}
+
+void floydWarshallManaged(thrust::host_vector<int>& h_vec)
+{
+    int * hostData = thrust::raw_pointer_cast(h_vec.data());
+    int N = sqrt(h_vec.size());
+    int size = N * N * sizeof(int);
+
+    // Track subtask time
+    auto timeInit = std::chrono::high_resolution_clock::now();
+    
+    int * cudaData;
+    cudaMallocManaged(&cudaData, size);
+    
+    // Device memory allocated
+    auto timeHtD = std::chrono::high_resolution_clock::now();
+    
+    for (int k = 0; k < N; k++) {
+        iterKernel<<< dim3(iDivUp(N, BLOCKSIZE), N), BLOCKSIZE >>> (k, cudaData, N);
+    }
+
+    // Calculations complete
+	auto timeExec = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double, std::milli> hostToDevice = timeHtD - timeInit;
+    std::cout << "Copying data from host to device took " << hostToDevice.count() << " ms." << std::endl;
+
+    std::chrono::duration<double, std::milli> exec = timeExec - timeHtD;
+    std::cout << "Executing calculations took " << exec.count() << " ms." << std::endl;
+}
---
+++ apps/floydWarshallManaged/floydWarshallManaged.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <thrust/host_vector.h>
+
+void floydWarshallManaged(thrust::host_vector<int>& h_vec);
